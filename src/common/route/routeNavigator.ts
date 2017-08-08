5de381200c94eb260d431066ea6c31bfbbbef44e
Tu Tran <contact@tranthanhtu.vn>
2017-08-08 16:53:55 +0700

    update routenavigator

--- src/common/route/routeNavigator.ts
+++ src/common/route/routeNavigator.ts
@@ -2,20 +2,22 @@ import pageHelper from "../helpers/pageHelper";
 export class RouteNavigator {
     public static routes: Array<any> = [];
     public static renderTo: string = "router";
+    public static currentRoute: any = null;
     public static registerRoutes(routes: Array<any>) {
         RouteNavigator.routes = routes.concat(RouteNavigator.routes);
     }
     public static navigate(url: string = ""): void {
-        let page = RouteNavigator.getPageByUrl(url);
-        pageHelper.render(RouteNavigator.renderTo, page);
+        let route = RouteNavigator.getRouteByUrl(url);
+        RouteNavigator.currentRoute = route;
+        pageHelper.render(RouteNavigator.renderTo, route.page);
     }
-    public static getPageByUrl(url: string) {
+    public static getRouteByUrl(url: string) {
         let route = RouteNavigator.routes.firstOrDefault((routeItem: any) => {
             return (String.isNullOrWhiteSpace(url) && routeItem.isDefault == true) || (routeItem.path == url);
         });
         if (!route) {
             throw String.format("path '{0}' was not registered.");
         }
-        return route.page;
+        return route;
     }
 }
